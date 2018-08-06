6579f288af72d2ae4ac7651250b9dc0e7f1ecd3a
Brian Bayat <brian.bayat@gmail.com>
2018-08-06 18:04:38 -0400

    Vegetable, Gardner, Plot tables created & data seeded into them

--- seed.js
+++ seed.js
@@ -72,3 +72,109 @@ Vegetable.create({
 
 
 
+Gardner.create({
+  name: 'John',
+  age: 20
+}).then(result => {
+  console.log('gardner created');
+})
+.catch(err => {
+  console.log(err);
+});
+
+Gardner.create({
+  name: 'Brian',
+  color: '21'
+})
+.then(result => {
+  console.log('seed created');
+})
+.catch(err => {
+  console.log(err);
+});
+Gardner.create({
+  name: 'Cory',
+  age: 22
+}).then(result => {
+  console.log('gardner created');
+})
+.catch(err => {
+  console.log(err);
+});
+Gardner.create({
+  name: 'Matt',
+  age: 23
+}).then(result => {
+  console.log('gardner created');
+})
+.catch(err => {
+  console.log(err);
+});
+Gardner.create({
+  name: 'Scott',
+  age: 24
+}).then(result => {
+  console.log('gardner created');
+})
+.catch(err => {
+  console.log(err);
+});
+
+
+
+Plot.create({
+  size: 1000
+}).then(result => {
+  console.log('plot created');
+})
+.catch(err => {
+  console.log(err);
+});
+
+Plot.create({
+  size: 2000,
+  shaded: false
+}).then(result => {
+  console.log('plot created');
+})
+.catch(err => {
+  console.log(err);
+});
+Plot.create({
+  size: 3000
+}).then(result => {
+  console.log('plot created');
+})
+.catch(err => {
+  console.log(err);
+});
+Plot.create({
+  size: 4000,
+  shaded: false
+}).then(result => {
+  console.log('plot created');
+})
+.catch(err => {
+  console.log(err);
+});
+Plot.create({
+  size: 5000
+}).then(result => {
+  console.log('plot created');
+})
+.catch(err => {
+  console.log(err);
+});
+Plot.create({
+  size: 6000,
+  shaded: false
+}).then(result => {
+  console.log('plot created');
+})
+.catch(err => {
+  console.log(err);
+});
+
+
+
+
